6ced51f6c201137929da421359284f8e84175b54
error-empire <timo.steuerwald@gmx.de>
2021-01-13 20:47:32 +0100

    mob next [ci-skip]

--- src/test/java/rgbledring/FailingTest.java
+++ src/test/java/rgbledring/FailingTest.java
@@ -3,12 +3,16 @@ package rgbledring;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 class FailingTest {
 
 	@Test
 	void test() {
 		assertThat("foo").isEqualTo("bar");
+	}
+
+	@Test
+	void testAnotherThing() {
+		//Sorry, second test for verifying that my git ssh setup is finally correct
+		assertThat("Timo").isEqualTo("error_empire");
 	}
 }
