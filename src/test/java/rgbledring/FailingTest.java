9094db06acb181ece2fbb0e489f08a8bfbac192f
Christoph Haefner <christoph.haefner@mailbox.org>
2021-01-13 19:48:49 +0100

    Squashed commit of the first setup-session (2020-01-13)
    commit a74b87803fed39cd5a0508310eb1526951097bb9
    Author: Christoph Haefner <christoph.haefner@mailbox.org>
    Date:   Wed Jan 13 19:45:32 2021 +0100
    
        mob next [ci-skip]
    
    commit ee0f5c386e15038cf2df56a657576680d2233c7f
    Author: Simon Siegler <simon@simon-siegler.de>
    Date:   Wed Jan 13 19:40:31 2021 +0100
    
        mob next [ci-skip]
    
    commit ccd0e0756b03c52334b48d201b43c774c5e99f59
    Author: Peter M. Clausen <petermichaelclausen@gmail.com>
    Date:   Wed Jan 13 19:32:29 2021 +0100
    
        mob next [ci-skip]
    
    commit 67150bf1389158a2c64ed8ea8e515921cb1d8415
    Author: Timo Steuerwald <timo.steuerwald@itk-engineering.de>
    Date:   Wed Jan 13 19:22:39 2021 +0100
    
        mob next [ci-skip]
    
    commit a0d73593f821591011b35f941bb0dafadac163ae
    Author: Daniel Brown <djbrown@users.noreply.github.com>
    Date:   Wed Jan 13 19:15:30 2021 +0100
    
        mob next [ci-skip]

--- src/test/java/rgbledring/FailingTest.java
+++ src/test/java/rgbledring/FailingTest.java
@@ -3,11 +3,12 @@ package rgbledring;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 class FailingTest {
 
 	@Test
 	void test() {
 		assertThat("foo").isEqualTo("bar");
 	}
-
 }
