f046bdd4ce81f4cbdb233235f847246d785c2045
Marc B <marc.burgard@web.de>
2021-01-20 21:21:44 +0100

    mob next [ci-skip]

--- src/test/java/rgbledring/LedRingsTest.java
+++ src/test/java/rgbledring/LedRingsTest.java
@@ -1,37 +1,45 @@
 package rgbledring;
 
+import java.util.function.IntPredicate;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LedRingsTest {
 	private boolean led1;
+	private boolean led2;
 
 	@Test
-	void testGivenRingOfSize16_whenLevelIsSetTo1_thenLED1IsOn() {
-		givenLEDRingOfSize16();
+	void testGivenRingOfSize1_whenLevelIsSetTo1_thenLED1IsOn() {
+		givenLEDRingOfSize(1);
 		whenLevelIsSetTo(1);
 		thenLED1IsOn();
 	}
 
 	@Test
-	void testGivenRingOfSize16_whenLevelIsSetTo0_thenLED1IsOff() {
-		givenLEDRingOfSize16();
+	void testGivenRingOfSize1_whenLevelIsSetTo0_thenLED1IsOff() {
+		givenLEDRingOfSize(1);
 		whenLevelIsSetTo(0);
 		thenLED1IsOff();
 	}
 	
 	@Test
-	void testGivenRingOfSize2_whenLevelIsSetTo51_thenLED1IsOff() {
-		givenLEDRingOfSize2();
-		whenLevelIsSetTo(0);
-		thenLED1IsOff();
+	void testGivenRingOfSize2_whenLevelIsSetTo51_thenLED1And2AreOn() {
+		givenLEDRingOfSize(2);
+		whenLevelIsSetTo(51);
+		thenLED1IsOn();
+		thenLED2IsOn();
+	}
+
+	private void thenLED2IsOn() {
+		Assertions.assertThat(led2).isTrue();
 	}
 
 	private void thenLED1IsOff() {
 		Assertions.assertThat(led1).isFalse();
 	}
 
-	private void givenLEDRingOfSize16() {
+	private void givenLEDRingOfSize(int size) {
 	}
 
 	private void whenLevelIsSetTo(int level) {
@@ -39,6 +47,10 @@ public class LedRingsTest {
 			led1 = false;
 		} else if (level == 1) {
 			led1 = true;
+		}
+		if (level > 50) {
+			led1 = true;
+			led2 = true;
 		}
 	}
 
