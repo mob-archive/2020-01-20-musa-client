71f7e506ebde1db58be918154123d114ac88bb8e
Timo Steuerwald <timo.steuerwald@gmx.de>
2021-01-20 21:52:06 +0100

    mob next [ci-skip]

--- src/test/java/rgbledring/LedRingsTest.java
+++ src/test/java/rgbledring/LedRingsTest.java
@@ -10,25 +10,25 @@ public class LedRingsTest {
 	void testGivenRingOfSize2_whenLevelIsSetTo51_thenLED1And2AreOn() {
 		givenLEDRingOfSize(2);
 		whenLevelIsSetTo(51);
-		thenLEDIsOn(0);
-		thenLEDIsOn(1);
+		thenLEDsAre(true, true);
+	}
+
+	private void thenLEDsAre(boolean... b) {
+		Assertions.assertThat(leds).isEqualTo(b);
 	}
 
 	@Test
 	void testGivenRingOfSize2_whenLevelIsSetTo0_thenLED1And2AreOff() {
 		givenLEDRingOfSize(2);
 		whenLevelIsSetTo(0);
-		thenLEDisOff(0);
-		thenLEDisOff(1);
+		thenLEDsAre(false, false);
 	}
-
 
 	@Test
 	void testGivenRingOfSize2_whenLevelIsSetTo0to50_thenLED1OnAndLed2Off() {
 		givenLEDRingOfSize(2);
 		whenLevelIsSetTo(2);
-		thenLEDIsOn(0);
-		thenLEDisOff(1);
+		thenLEDsAre(true, false);
 	}
 
 	private void thenLEDIsOn(int index) {
