af1cdc92129473545ae2633019a451081bcba7c6
Marc B <marc.burgard@web.de>
2021-01-20 20:20:16 +0100

    mob next [ci-skip]

--- src/main/java/rgbledring/RGBLEDRing.java
+++ src/main/java/rgbledring/RGBLEDRing.java
@@ -1,7 +1,13 @@
 package rgbledring;
 
 public class RGBLEDRing {
+	
+	public RGBLEDRing(int size) {
+	}
     public boolean slider1Value(int input) {
+    	if (input == 0) {
+			return false;
+		}
 		return true;
     }
 }
--- src/test/java/rgbledring/RGBLEDRingTest.java
+++ src/test/java/rgbledring/RGBLEDRingTest.java
@@ -4,6 +4,8 @@ package rgbledring;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -13,11 +15,21 @@ public class RGBLEDRingTest {
     @Test
     public void testFirstLedIsOn() {
     	
-    	RGBLEDRing ring = new RGBLEDRing();
+    	RGBLEDRing ring = new RGBLEDRing(1);
     	
         boolean led1On = ring.slider1Value(5);
         
         assertTrue(led1On);
+    }
+    
+    @Test
+    public void testFirstLedIsOff() {
+    	
+    	RGBLEDRing ring = new RGBLEDRing(1);
+    	
+    	boolean led1Off = ring.slider1Value(0);
+    	
+    	assertThat(led1Off).isFalse();
     }
 
 }
